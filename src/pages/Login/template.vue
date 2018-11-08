3e0ca1e1b01cf89895091c5bc0e960f459e98e98
Keller <381796751@qq.com>
2018-11-08 17:10:12 +0800

    静态页面完成

--- src/pages/Login/template.vue
+++ src/pages/Login/template.vue
@@ -1,9 +1,18 @@
 <template>
 	<div id="login">
-		<p>登录页面</p>
+		<h4>用户名</h4>
+		<el-input v-model="username" placeholder="用户名"></el-input>
+		<p class="error">当前用户名已注册</p>
+		<h4>密码</h4>
+		<el-input v-model="password" type="password" placeholder="密码"></el-input>
+		<p class="error">当前用户名已注册</p>
+		<el-button size="medium">立即登录</el-button>
+		<p class="notice">没有账号？
+			<router-link to="/register">注册新用户</router-link>
+		</p>
 	</div>
 </template>
 
 <script src="./template.js"></script>
 
-<style src="./template.css"></style>
+<style src="./template.less" lang="less"></style>
