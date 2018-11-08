a80a9093cc45bcbfc04a66fc57a4385fa9c404e4
Keller <381796751@qq.com>
2018-11-08 13:46:09 +0800

    添加header、footer组件

--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,9 +1,6 @@
 <template>
 	<div id="index">
 		<h1>首页</h1>
-		<el-button type="primary" @click="onclick">主要按钮</el-button>
-		<el-input v-model="input" placeholder="请输入内容"></el-input>
-		<el-slider v-model="value1"></el-slider>
 	</div>
 </template>
 
