f5a73fc0df51a415a83b5b3c35353aed8de1197b
Keller <381796751@qq.com>
2018-11-09 17:11:22 +0800

    路由完善、异步加载、权限控制

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,119 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+/*
 import Index from '@/pages/Index/template.vue'
 import Login from '@/pages/Login/template.vue'
-import Register from '@/pages/Register/template.vue'
 import Detail from '@/pages/Detail/template.vue'
-import Create from '@/pages/Create/template.vue'
 import Edit from '@/pages/Edit/template.vue'
-import My from '@/pages/My/template.vue'
+import Create from '@/pages/Create/template.vue'
+import Register from '@/pages/Register/template.vue'
 import User from '@/pages/User/template.vue'
+import My from '@/pages/My/template.vue'
+*/
 
+import store from '../store'
+window.store = store
 
 Vue.use(Router)
 
-export default new Router({
+/*
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      component: Index
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/register',
+      component: Register
+    },
+    {
+      path: '/detail/:blogId',
+      component: Detail
+    },
+    {
+      path: '/edit/:blogId',
+      component: Edit,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/create',
+      component: Create,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/user/:userId',
+      component: User
+    },
+    {
+      path: '/my',
+      component: My,
+      meta: { requiresAuth: true }
+    }
+  ]
+})
+*/
+
+const router = new Router({
 	routes: [
 		{
 			path: '/',
-			component: Index
+			component: () => import('@/pages/Index/template.vue')
 		},
 		{
 			path: '/login',
-			component: Login
+			component: () => import('@/pages/Login/template.vue')
 		},
 		{
-			path: '/register',
-			component: Register
+			path: '/detail/:blogId',
+			component: () => import('@/pages/Detail/template.vue')
 		},
 		{
-			path: '/detail',
-			component: Detail
+			path: '/edit/:blogId',
+			component: () => import('@/pages/Edit/template.vue'),
+			meta: { requiresAuth: true }
 		},
 		{
 			path: '/create',
-			component: Create
+			component: () => import('@/pages/Create/template.vue'),
+			meta: { requiresAuth: true }
 		},
 		{
-			path: '/edit',
-			component: Edit
+			path: '/user/:userId',
+			component: () => import('@/pages/User/template.vue')
 		},
 		{
 			path: '/my',
-			component: My
+			component: () => import('@/pages/My/template.vue'),
+			meta: { requiresAuth: true }
 		},
 		{
-			path: '/user',
-			component: User
-		},
+			path: '/register',
+			component: () => import('@/pages/Register/template.vue')
+		}
 	]
 })
+
+router.beforeEach((to, from, next) => {
+	if (to.matched.some(record => record.meta.requiresAuth)) {
+		store.dispatch('checkLogin').then(isLogin => {
+			if (!isLogin) {
+				next({
+					path: '/login',
+					query: { redirect: to.fullPath }
+				})
+			} else {
+				next()
+			}
+		})
+	} else {
+		next() // 确保一定要调用 next()
+	}
+})
+
+export default router
+
